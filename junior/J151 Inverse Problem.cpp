566e73540dc7bcd366ce161cc90d1fff6c0bd79d
54D <ArceusMaster0493@users.noreply.github.com>
2017-11-18 22:08:06 +0800

    Update J151 Inverse Problem.cpp

--- junior/J151 Inverse Problem.cpp
+++ junior/J151 Inverse Problem.cpp
@@ -1,26 +1,30 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 int main(){
     int n,sum;
     cin>>n;
     cin>>sum;
-    int left,consist[n];
+    double left,consist[n+1];
     left=sum;
     for(int i=0;i<n-1;i++){
         consist[i]=i+1;
         left=left-(i+1);
     }
     consist[n-1]=left;
-    if(!(consist[n-1]>n-1)){
-        int change=consist[n-1]+1;
+    if(!(left>n-1)&&left>0){
+        double change=consist[n-1]+1;
         consist[n-1]=consist[n-1]-change;
         consist[n-2]=consist[n-2]+change;
-    }else if(consist[n-1]==0){
-        consist[n-2]=consist[n-2]+1;
-        consist[n-1]=-1;
     }
-    for(int i=0;i<n;i++){
-        cout<<consist[i]<<" ";
+    if(n==1&&sum==0){
+        cout<<"0";
+    }
+    else{
+        for(int i=0;i<n;i++){
+            cout<<fixed<<setprecision(0);
+            cout<<consist[i]<<" ";
+        }
     }
     return 0;
 }
