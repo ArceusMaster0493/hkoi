9b111c89f4bdea53dcab24c9bbffa597ccce0cde
54D <ArceusMaster0493@users.noreply.github.com>
2017-12-07 12:38:14 +0800

    Create D401 Sieve of Eratosthenes.java

---
+++ dse/D401 Sieve of Eratosthenes.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class playground{
+    public static void main(String args[]){
+        Map<Integer,Boolean> sieve = new HashMap<Integer,Boolean>();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=2;i<N+1;i++){
+            sieve.put(i,true);
+        }
+        for(int i=2;i<N+1;i++){
+            if(sieve.get(i)==true){
+                int per = i;
+                for(int j=i;j<N+1;j+per){
+                    sieve.put(j,false);
+                }
+                System.out.printf(sieve.get(i));
+            }
+        }
+        sc.close();
+    }
+}
