5e7bf21b5feb40ad80eafc162eb246a049376ae9
54D <ArceusMaster0493@users.noreply.github.com>
2018-02-24 13:02:10 +0800

    Update D401 Sieve of Eratosthenes.java

--- dse/D401 Sieve of Eratosthenes.java
+++ dse/D401 Sieve of Eratosthenes.java
@@ -19,3 +19,39 @@ class playground{
         sc.close();
     }
 }
+
+// Steven's
+
+/*
+
+import java.util.*;
+public class playground{
+    public static void main(String args[]){
+        Map<Integer,Boolean> sieve = new HashMap<Integer,Boolean>();
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=3;i<N+1;i+=2){
+            sieve.put(i,true);
+        }
+        System.out.printf("2\n");
+        
+        for(int i=3;i<N+1&&i<1000;i+=2){
+            if(sieve.get(i)==true){
+                for(int j=i*i;j<N+1;j+=2*i){
+                    sieve.put(j,false);
+                }
+                System.out.println(i);
+            }
+        }
+        for(int i=1001;i<N;i+=4){
+            if(sieve.get(i)==true){
+                System.out.println(i);
+            }
+            i+=2;
+             if(sieve.get(i)==true){
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
+}
