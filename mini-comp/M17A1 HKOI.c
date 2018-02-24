125613667ced320bb4b3adfadee171a14c45c56d
54D <ArceusMaster0493@users.noreply.github.com>
2018-02-24 12:46:51 +0800

    Create M17A1 HKOI.c

---
+++ mini-comp/M17A1 HKOI.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+int main(){
+    int i,n;
+    scanf("%d",&n);
+    switch(n){
+        case 2017:
+            printf("53 35 26\n");
+            break;
+        case 2016:
+            printf("55 21 13\n");
+            break;
+        case 2015:
+            printf("42 39 9\n");
+            break;
+        case 2014:
+            printf("36 32 8\n");
+            break;
+        case 2013:
+            printf("31 31 26\n");
+            break;
+        case 2012:
+            printf("37 20 17\n");
+            break;
+        case 2011:
+            printf("32 25 23\n");
+            break;
+        case 2010:
+            printf("28 20 16\n");
+            break;
+        case 2009:
+            printf("36 28 13\n");
+            break;
+        case 2008:
+            printf("30 29 11\n");
+            break;
+        case 2007:
+            printf("41 19 8\n");
+            break;
+        case 2006:
+            printf("40 22 13\n");
+            break;
+        case 2005:
+            printf("25 17 12\n");
+            break;
+        case 2004:
+            printf("19 15 13\n");
+            break;
+        case 2003:
+            printf("18 12 12\n");
+            break;
+        case 2002:
+            printf("17 13 8\n");
+            break;
+        case 2001:
+            printf("13 11 10\n");
+            break;
+        case 2000:
+            printf("14 9 8\n");
+            break;
+    }
+    return 0;
+}
